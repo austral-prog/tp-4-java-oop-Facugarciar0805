1a77343e37d7e576d2232e2ff8d6a9828b56be57
Facundo Garcia Rosselli <facugarciar@gmail.com>
2024-08-30 13:02:54 -0300

    Entrego

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,6 +1,42 @@
 package com.template;
 
 public class Elevator {
+    int min;
+    int max;
+    int current;
+    int passengers;
+    int maxpassangers;
+    public Elevator(int min, int max, int maxpassengers) {
+        this.min = min;
+        this.max = max;
+        this.current = 0;
+        this.passengers = 0;
+        this.maxpassangers = maxpassengers;
+    }
+    public void moveToFloor(int piso){
+        if ((piso<=this.max) && (piso>=this.min )){
+            this.current = piso;
+        }
+    }
+    public int getCurrentFloor() {
+        return this.current;
+    }
+    public int getPassengerCount() {
+        return this.passengers;
+    }
+    public void addPassenger(){
+       if(this.passengers<this.maxpassangers){
+           this.passengers++;
+       }
+
+    }
+    public void removePassenger(){
+        if (this.passengers > 0){
+            passengers--;
+        }
+    }
+
+
 
 
 
